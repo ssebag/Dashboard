a64476ff1a2b24ac9923813c3ed1502be2fa649a
seba-gh <50120833+ssebag@users.noreply.github.com>
2024-08-21 19:29:53 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Sidebar from './layout/Sidebar/Sidebar'
 import Content from './layout/Content/Content'
+import {DarkModeProvider} from './context/DarkModeContext';
 
 function App() {
   
   return (
-    <div className='app'>
-      <Sidebar />
-      <Content />
-    </div>
+     <DarkModeProvider >  
+        <div className='app'>
+           <Sidebar />
+           <Content />
+        </div>
+     </DarkModeProvider >  
   )
 }
 
 export default App
+
